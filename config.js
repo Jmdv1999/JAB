3175ee4c3ad8a8f6e0fbc876b02ab01786467792
Jose Miguel Delgado Villasmil <91023157+Jmdv1999@users.noreply.github.com>
2024-07-30 08:53:02 -0400

    Eliminamos el token del config.js para hacer el repo publico

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const token = "MTExNTQzMjE0NjAxOTAzMzE1OQ.G8Dvoi.Nzdyq3k7f96WJFy2OHsEzAwVlNY7ndxwaMXcd0"
+export const token = ""
 
-export const client_id = "1115432146019033159"
+export const client_id = ""
